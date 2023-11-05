852c2387acb6d3d1619ccc6984bd17a0e08f75ee
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-05 14:30:10 -0300

    add validation candidate entity

--- src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cleilsonandrade.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidates")
 public class CandidateController {
 
   @PostMapping
-  public void create(@RequestBody CandidateEntity candidateEntity) {
+  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
 
   }
 }
