85a31cd9b41589e9d2d3de7f221da8dd5e38e999
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-05 17:15:03 -0300

    implementation CandidateRepository in controller

--- src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,19 +1,24 @@
 package br.com.cleilsonandrade.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cleilsonandrade.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.cleilsonandrade.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidates")
 public class CandidateController {
 
-  @PostMapping
-  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+  @Autowired
+  private CandidateRepository candidateRepository;
 
+  @PostMapping
+  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    return this.candidateRepository.save(candidateEntity);
   }
 }
