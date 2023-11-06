0d30e6143ed20079ac652f2ee5ba8c9b5aaa9163
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-05 22:13:42 -0300

    create JobController and implementation

---
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/controllers/JobController.java
@@ -0,0 +1,21 @@
+package br.com.cleilsonandrade.gestao_vagas.modules.company.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cleilsonandrade.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.cleilsonandrade.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+  @Autowired
+  private CreateJobUseCase createJobUseCase;
+
+  public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
+    return this.createJobUseCase.execute(jobEntity);
+  }
+}
