cd5033b0d123afc76a5c471b623f9a633355c122
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-05 22:07:57 -0300

    create CreateJobUseCase

---
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -0,0 +1,17 @@
+package br.com.cleilsonandrade.gestao_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cleilsonandrade.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.cleilsonandrade.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class CreateJobUseCase {
+  @Autowired
+  private JobRepository jobRepository;
+
+  public JobEntity execute(JobEntity jobEntity) {
+    return this.jobRepository.save(jobEntity);
+  }
+}
