eacd8897059232250061d10c93fe0eabb01e7aed
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-05 21:28:01 -0300

    add validation to create company existing

--- src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/repositories/CompanyRepository.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -1,5 +1,6 @@
 package br.com.cleilsonandrade.gestao_vagas.modules.company.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.cleilsonandrade.gestao_vagas.modules.company.entities.CompanyEntity;
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
-
+  Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
 }
--- src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -3,6 +3,7 @@ package br.com.cleilsonandrade.gestao_vagas.modules.company.useCases;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.cleilsonandrade.gestao_vagas.exceptions.UserFoundException;
 import br.com.cleilsonandrade.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.cleilsonandrade.gestao_vagas.modules.company.repositories.CompanyRepository;
 
@@ -11,7 +12,12 @@ public class CreateCompanyUseCase {
   @Autowired
   private CompanyRepository companyRepository;
 
-  private void execute(CompanyEntity companyEntity) {
-    this.companyRepository.save(companyEntity);
+  public CompanyEntity execute(CompanyEntity companyEntity) {
+    this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
+        .ifPresent((user) -> {
+          throw new UserFoundException();
+        });
+
+    return this.companyRepository.save(companyEntity);
   }
 }
