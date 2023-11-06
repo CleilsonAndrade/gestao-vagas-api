d5d4d0b0131d58421a38354353f0752bedc86e8c
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-06 11:07:53 -0300

    add method to encryption passw company

--- src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,6 +1,7 @@
 package br.com.cleilsonandrade.gestao_vagas.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.cleilsonandrade.gestao_vagas.exceptions.UserFoundException;
@@ -12,11 +13,17 @@ public class CreateCompanyUseCase {
   @Autowired
   private CompanyRepository companyRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public CompanyEntity execute(CompanyEntity companyEntity) {
     this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
         .ifPresent((user) -> {
           throw new UserFoundException();
         });
+
+    var password = passwordEncoder.encode(companyEntity.getPassword());
+    companyEntity.setPassword(password);
 
     return this.companyRepository.save(companyEntity);
   }
--- src/main/java/br/com/cleilsonandrade/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package br.com.cleilsonandrade.gestao_vagas.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +20,9 @@ public class SecurityConfig {
         });
 
     return httpSecurity.build();
+  }
+
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
