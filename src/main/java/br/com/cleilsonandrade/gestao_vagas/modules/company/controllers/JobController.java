73bb0d8e367cf8e26888841653a3576b0de1542e
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-05 22:15:56 -0300

    add bean Post in create route

--- src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,6 +1,7 @@
 package br.com.cleilsonandrade.gestao_vagas.modules.company.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class JobController {
   @Autowired
   private CreateJobUseCase createJobUseCase;
 
+  @PostMapping
   public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
     return this.createJobUseCase.execute(jobEntity);
   }
