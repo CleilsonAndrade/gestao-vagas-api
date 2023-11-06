1339ccf42d52ad4b72e46f7e087775d80b3e4609
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-05 21:41:12 -0300

    add Valid bean in route create

--- src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/controllers/CompanyController.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cleilsonandrade.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.cleilsonandrade.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/companies")
@@ -17,7 +18,7 @@ public class CompanyController {
   private CreateCompanyUseCase createCompanyUseCase;
 
   @PostMapping
-  public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity) {
+  public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity companyEntity) {
     try {
       var result = this.createCompanyUseCase.execute(companyEntity);
       return ResponseEntity.ok().body(result);
