8fcb428075603c87319350fcbd84a38a25460deb
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-06 10:53:02 -0300

    implementation SpringSecurity

---
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package br.com.cleilsonandrade.gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> {
+          auth.requestMatchers("/candidates/").permitAll()
+              .requestMatchers("/companies/").permitAll();
+          auth.anyRequest().authenticated();
+        });
+
+    return httpSecurity.build();
+  }
+}
