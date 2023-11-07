1302695e6f1a22ae5c094918c87d4b7ac729603f
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-07 11:53:13 -0300

    created SecurityConfig and Filter

---
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/providers/JWTProvider.java
@@ -0,0 +1,31 @@
+package br.com.cleilsonandrade.gestao_vagas.providers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+@Service
+public class JWTProvider {
+  @Value("${security.token}")
+  private String secretKey;
+
+  public String validateToken(String token) {
+    token = token.replace("Bearer ", "");
+
+    Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+    try {
+      var subject = JWT.require(algorithm)
+          .build()
+          .verify(token)
+          .getSubject();
+
+      return subject;
+    } catch (JWTVerificationException e) {
+      return "";
+    }
+  }
+}
--- src/main/java/br/com/cleilsonandrade/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/security/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
     return httpSecurity.build();
   }
 
+  @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
