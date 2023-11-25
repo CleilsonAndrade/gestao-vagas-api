b3dc3df3e179e6835722fd41a971da18d5163953
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-24 23:12:40 -0300

    feat: generate token candidate

--- src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
@@ -9,4 +9,6 @@ import br.com.cleilsonandrade.gestao_vagas.modules.candidate.CandidateEntity;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
   Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+
+  Optional<CandidateEntity> findByUsername(String username);
 }
---
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,55 @@
+package br.com.cleilsonandrade.gestao_vagas.modules.candidate.useCases;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.cleilsonandrade.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.cleilsonandrade.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+import br.com.cleilsonandrade.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+
+@Service
+public class AuthCandidateUseCase {
+  @Value("${security.token.secret.candidate}")
+  private String secretKey;
+
+  @Autowired
+  private CandidateRepository candidateRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO)
+      throws AuthenticationException {
+    var candidate = candidateRepository.findByUsername(authCandidateRequestDTO.username()).orElseThrow(() -> {
+      throw new UsernameNotFoundException("Username or password incorrect");
+    });
+
+    var passwordMatches = this.passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+    if (!passwordMatches) {
+      throw new AuthenticationException();
+    }
+
+    Algorithm algorithm = Algorithm.HMAC256(secretKey);
+    var token = JWT.create()
+        .withIssuer("cleilsonandrade")
+        .withSubject(candidate.getId().toString())
+        .withClaim("roles", Arrays.asList("candidate"))
+        .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+        .sign(algorithm);
+
+    return AuthCandidateResponseDTO.builder().access_token(token).build();
+  }
+}
