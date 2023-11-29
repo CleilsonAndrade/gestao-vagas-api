736e70083cb6ae7758c92b2120234b7688feb7e0
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-29 15:32:27 -0300

    refactor: add config to swagger

--- src/main/java/br/com/cleilsonandrade/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/cleilsonandrade/gestao_vagas/GestaoVagasApplication.java
@@ -3,15 +3,7 @@ package br.com.cleilsonandrade.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Gestão de Vagas", description = "API for vacancy management", version = "1"))
-@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
