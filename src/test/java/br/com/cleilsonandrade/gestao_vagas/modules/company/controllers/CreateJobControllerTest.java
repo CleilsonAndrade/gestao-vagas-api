9ad223bc21a40a7841ff59ce004fdf5b8715fe56
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-02-20 16:30:44 -0300

    🔨 build: add h2database and configurations

--- src/test/java/br/com/cleilsonandrade/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/cleilsonandrade/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,6 +23,7 @@ import br.com.cleilsonandrade.gestao_vagas.utils.TestUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateJobControllerTest {
 
   private MockMvc mvc;
